67286eb1e5562f92e6a808bc34b2d4f572409f71
nicolasburgueno <nburgueno.dev@hotmail.com>
2024-04-12 15:12:53 -0300

    feat: navbar and home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
+import Home from './pages/Home/Home';
 import Login from './pages/Login';
 import { Route, Routes } from 'react-router';
+import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Login />}/>
+        <Route path="/" element={<Login />} />
+        <Route exact path="/home" element={<Home />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
